888344b7e33bfbdb60bf940ceeac8a2ac21afa0a
Соджиро <koyama10032009@outlook.jp>
2022-08-14 14:57:32 +0900

    abc261 - d

--- abc/src/abc261.rs
+++ abc/src/abc261.rs
@@ -1,12 +1,52 @@
-use std::collections::HashMap;
-
 use itertools::Itertools;
 use proconio::{
     fastout, input,
     marker::{Bytes, Chars, Isize1, Usize1},
 };
+use std::collections::HashMap;
+#[allow(dead_code)]
+fn update<T: PartialOrd>(value: T, target: &mut T, cond: std::cmp::Ordering) {
+    if value.partial_cmp(target) == Some(cond) {
+        *target = value;
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! chmax {
+    ($target:expr, $value:expr) => {
+        update($value, &mut $target, std::cmp::Ordering::Greater)
+    };
+}
+#[allow(unused_macros)]
+macro_rules! chmin {
+    ($target:expr, $value:expr) => {
+        update($value, &mut $target, std::cmp::Ordering::Less)
+    };
+}
+
 pub fn main() {
-    c()
+    d()
+}
+
+fn d() {
+    input! {n:usize,m:usize,x: [usize;n],d: [(usize,usize);m]}
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+    let mut bonus = vec![0; n + 1];
+    for (c, y) in d {
+        bonus[c] += y;
+    }
+
+    for i in 1..n + 1 {
+        for j in 1..=i {
+            chmax!(dp[i][0], dp[i - 1][j]);
+            dp[i][j] = dp[i - 1][j - 1] + x[i - 1] + bonus[j];
+        }
+    }
+    let mut max = 0;
+    for i in 1..=n {
+        chmax!(max, dp[n][i]);
+    }
+    println!("{}", max);
 }
 
 fn c() {
