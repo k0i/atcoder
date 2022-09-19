9374f4eae44ee3866885d9ca6f6f82b041a8b6dc
Соджиро <koyama10032009@outlook.jp>
2022-09-19 14:08:55 +0900

    abc269 - d

--- abc/abc269/src/bin/d.rs
+++ abc/abc269/src/bin/d.rs
@@ -11,41 +11,103 @@ pub fn main() {
     n:usize,
     xy:[(i64,i64);n]
             }
-    let mut visited = HashSet::new();
-    let mut ans = n;
-    for x in 0..n {
-        let (i, j) = xy[x];
-        visited.insert((i, j));
-        for y in x + 1..n {
-            let (k, l) = xy[y];
-            if k == i && l == j {
-                ans -= 1;
-            }
-            if k == i - 1 && l == j - 1 && !visited.contains(&(i - 1, j - 1)) {
-                ans -= 1;
-                visited.insert((k, l));
-            }
-            if k == i - 1 && l == j + 1 && !visited.contains(&(i - 1, j + 1)) {
-                ans -= 1;
-                visited.insert((k, l));
-            }
-            if k == i - 1 && l == j - 1 && !visited.contains(&(i - 1, j - 1)) {
-                ans -= 1;
-                visited.insert((k, l));
-            }
-            if k == i && l == j + 1 && !visited.contains(&(i, j + 1)) {
-                ans -= 1;
-                visited.insert((k, l));
-            }
-            if k == i + 1 && l == j + 1 && !visited.contains(&(i + 1, j + 1)) {
-                ans -= 1;
-                visited.insert((k, l));
-            }
-            if k == i + 1 && l == j + 1 && !visited.contains(&(i + 1, j + 1)) {
-                ans -= 1;
-                visited.insert((k, l));
+    let mut uf = UnionFindTree::new(n);
+    let coor = vec![(-1, -1), (-1, 0), (0, -1), (0, 1), (1, 0), (1, 1)];
+    for i in 0..n {
+        let (ax, ay) = xy[i];
+        for j in i + 1..n {
+            let (bx, by) = xy[j];
+            if coor.contains(&(ax - bx, ay - by)) {
+                uf.unite(i, j);
             }
         }
     }
-    println!("{:?}", ans);
+    let mut visited = HashSet::new();
+    let mut ans = 0;
+    for i in 0..n {
+        let r = uf.root(i);
+        if visited.contains(&r) {
+            continue;
+        }
+        visited.insert(r);
+        ans += 1;
+    }
+    println!("{}", ans);
+}
+
+/// Given the following tree structure
+///         0              7       9
+///        / \            /       / \
+///       1   3          8       10  11
+///      /   / \                      \
+///     2   4   5                      12
+///    /
+///   6
+/// ```
+/// use k0i::union_find::UnionFindTree;
+/// let tree = vec![(0,1),(0,3),(1,2),(3,4),(3,5),(2,6),(7,8),(9,10),(9,11),(11,12)];
+/// let mut uft = UnionFindTree::new(13);
+/// for (a,b) in tree{
+///     uft.unite(a,b);
+/// }
+/// assert!(uft.is_same(6,5));
+/// assert!(uft.is_same(6,4));
+/// assert!(uft.is_same(0,4));
+/// assert!(uft.is_same(2,5));
+/// assert!(uft.is_same(7,8));
+/// assert!(uft.is_same(9,11));
+///
+/// assert_eq!(uft.is_same(0,9),false);
+/// assert_eq!(uft.is_same(7,12),false);
+///
+/// assert_eq!(uft.size(0),7);
+/// assert_eq!(uft.size(7),2);
+/// assert_eq!(uft.size(9),4);
+/// ```
+
+#[derive(Debug)]
+pub struct UnionFindTree {
+    parents: Vec<isize>,
+    size: Vec<usize>,
+}
+
+impl UnionFindTree {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parents: vec![-1; n],
+            size: vec![1; n],
+        }
+    }
+    pub fn root(&mut self, x: usize) -> usize {
+        if self.parents[x] == -1 {
+            x
+        } else {
+            self.parents[x] = self.root(self.parents[x] as usize) as isize;
+            self.parents[x] as usize
+        }
+    }
+
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+    pub fn unite(&mut self, x: usize, y: usize) -> bool {
+        let mut x = self.root(x);
+        let mut y = self.root(y);
+
+        if x == y {
+            return false;
+        }
+
+        if self.size[x] < self.size[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.parents[y] = x as isize;
+        self.size[x] += self.size[y];
+        true
+    }
+
+    pub fn size(&mut self, x: usize) -> usize {
+        let r = self.root(x);
+        self.size[r]
+    }
 }
