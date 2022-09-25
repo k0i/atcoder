be4f1fe55e9e14167c6b9a27e58c0836c47423b4
Соджиро <koyama10032009@outlook.jp>
2022-09-25 11:25:59 +0900

    abc270 - d

--- abc/abc270/src/bin/d.rs
+++ abc/abc270/src/bin/d.rs
@@ -3,37 +3,41 @@ use proconio::{
     fastout, input,
     marker::{Bytes, Chars, Isize1, Usize1},
 };
-#[fastout]
-pub fn main() {
-    input! {mut n:usize,k:usize,a:[usize;k]}
-    let mut takahashi = 0;
-    let mut len = k - 1;
-    loop {
-        if n == 0 {
-            break;
-        }
-        let mut max = a[len];
-        if n < max {
-            len -= 1;
-            continue;
-        }
-        takahashi += max;
-        n -= max;
-        if n == 0 {
-            break;
-        }
-        if n >= max {
-            n -= max;
-            continue;
-        }
-        len -= 1;
-        let mut aoki_max = a[len];
-        while aoki_max > n {
-            len -= 1;
-            aoki_max = a[len];
-        }
-        len += 1;
-        n -= aoki_max;
+#[allow(dead_code)]
+fn update<T: PartialOrd>(value: T, target: &mut T, cond: std::cmp::Ordering) {
+    if value.partial_cmp(target) == Some(cond) {
+        *target = value;
     }
-    println!("{}", takahashi);
+}
+#[allow(unused_macros)]
+macro_rules! chmax {
+    ($target:expr, $value:expr) => {
+        update($value, &mut $target, std::cmp::Ordering::Greater)
+    };
+}
+#[allow(unused_macros)]
+macro_rules! chmin {
+    ($target:expr, $value:expr) => {
+        update($value, &mut $target, std::cmp::Ordering::Less)
+    };
+}
+#[fastout]
+fn main() {
+    input! {
+        mut n:usize,k:usize,a:[usize;k]
+    }
+    let mut dp = vec![0; n + 1];
+    for i in 0..=n {
+        for &r in a.iter() {
+            if i == r {
+                dp[i] = r;
+                break;
+            }
+            if i < r {
+                break;
+            }
+            chmax!(dp[i], i - dp[i - r]);
+        }
+    }
+    println!("{}", dp[n]);
 }
