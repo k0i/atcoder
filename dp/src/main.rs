cdeeac8b525a058ff1a5fab4e1b79e0efd325d32
Соджиро <koyama10032009@outlook.jp>
2022-10-06 15:48:59 +0900

    dp - i

--- dp/src/main.rs
+++ dp/src/main.rs
@@ -26,7 +26,30 @@ macro_rules! chmin {
 }
 
 pub fn main() {
-    h()
+    i()
+}
+
+fn i() {
+    input! {
+    n:usize,
+    p:[f64;n]
+    }
+    let mut dp = vec![vec![0.0; n + 1]; n + 1];
+    dp[0][0] = 1.0;
+    for i in 1..n + 1 {
+        for j in 0..=i {
+            dp[i][j] = dp[i - 1][j] * (1.0 - p[i - 1]);
+            if j > 0 {
+                dp[i][j] += dp[i - 1][j - 1] * p[i - 1];
+            }
+        }
+    }
+    let mut ans = 0.0;
+    for i in (n / 2 + 1)..n + 1 {
+        ans += dp[n][i];
+    }
+    println!("{:?}", dp);
+    println!("{}", ans);
 }
 
 fn h() {
