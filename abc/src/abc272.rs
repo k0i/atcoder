549e16ca90e53fcee517d348a068b16bfe65abb1
Соджиро <koyama10032009@outlook.jp>
2022-10-11 12:22:47 +0900

    abc272 - d

--- abc/src/abc272.rs
+++ abc/src/abc272.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use itertools::Itertools;
 use proconio::{
     fastout, input,
@@ -5,6 +7,49 @@ use proconio::{
 };
 #[fastout]
 pub fn main() {
+    input! {
+    n:usize,
+    m:i64
+    }
+    let mut d = vec![];
+    let mut a = -1001;
+    loop {
+        let mut b = -1001;
+        while b <= 1000 {
+            if a * a + b * b == m {
+                d.push((a, b));
+            }
+            b += 1;
+        }
+        a += 1;
+        if a > 1000 {
+            break;
+        }
+    }
+    let mut map = vec![vec![-1; n]; n];
+    map[0][0] = 0;
+    let mut queue = VecDeque::new();
+    queue.push_back((0, 0));
+    while let Some((x, y)) = queue.pop_front() {
+        for (dx, dy) in d.iter() {
+            let nx = x + dx;
+            let ny = y + dy;
+            if nx >= 0 && nx < n as i64 && ny >= 0 && ny < n as i64 {
+                if map[nx as usize][ny as usize] == -1 {
+                    map[nx as usize][ny as usize] = map[x as usize][y as usize] + 1;
+                    queue.push_back((nx, ny));
+                }
+            }
+        }
+    }
+    for i in 0..n {
+        for j in 0..n {
+            print!("{} ", map[i][j]);
+        }
+        println!();
+    }
+}
+fn c() {
     input! {
     n:usize,
     mut a:[i64;n]
