5877f81f22250db7617c33bed445d1203c4df76a
соджиро кояма <koyama10032009@outlook.jp>
2022-10-27 07:36:47 +0300

    abc252 - c

--- abc/src/abc252.rs
+++ abc/src/abc252.rs
@@ -7,6 +7,37 @@ use proconio::{
 };
 #[fastout]
 pub fn main() {
+    input! {
+        n: i32,
+        a: [Chars; n],
+    }
+    let rd: Vec<Vec<u8>> = a
+        .into_iter()
+        .map(|a| a.into_iter().map(|aa| aa as u8 - b'0').collect::<Vec<u8>>())
+        .collect();
+    let mut ans = 10000;
+    for i in 0usize..10usize {
+        let mut tmp = 0;
+        let mut used = vec![false; 10001];
+        for r in rd.iter() {
+            let mut num = 0;
+            for (ii, ri) in r.iter().enumerate() {
+                if *ri as usize == i {
+                    num = ii;
+                    break;
+                }
+            }
+            while used[num] {
+                num += 10
+            }
+            used[num] = true;
+            tmp = tmp.max(num);
+        }
+        ans = ans.min(tmp);
+    }
+    println!("{}", ans);
+}
+fn d() {
     input! {n:usize,
     a:[usize;n],
     }
