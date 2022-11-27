8c36c5ab2f3a8c38a52ede4951584665ee4b7c0d
соджиро кояма <koyama10032009@outlook.jp>
2022-11-28 00:07:35 +0300

    abc279 - d

--- abc/abc279/src/bin/d.rs
+++ abc/abc279/src/bin/d.rs
@@ -1,25 +1,16 @@
-use itertools::Itertools;
-use proconio::{
-    fastout, input,
-    marker::{Bytes, Chars, Isize1, Usize1},
-};
-#[fastout]
-pub fn main() {
+use proconio::input;
+use proconio::marker::*;
+fn main() {
     input! {
-    a:f64,
-      b:f64
-        }
-    let mut right = a as usize;
-    let mut left = 0;
-    let mut ans = a;
-    while right - left > 1 {
-        let mid = ((right + left) / 2) as f64;
-        if b * (mid - 1.) + a / mid.sqrt() < b * (mid) + a / (mid + 1.).sqrt() {
-            right = mid as usize;
-        } else {
-            left = mid as usize;
-        }
+        a: f64,
+        b: f64
+    };
+    let g = (a / 2.0 / b).powf(2.0 / 3.0).ceil();
+
+    let mut answer = std::f64::MAX;
+
+    for i in -5..6 {
+        answer = answer.min(a / (g + i as f64).sqrt() + b * (g + i as f64 - 1.0));
     }
-    ans = ans.min(b * (right as f64 - 1.) + a / (right as f64).sqrt());
-    println!("{}", ans);
+    println!("{}", answer)
 }
