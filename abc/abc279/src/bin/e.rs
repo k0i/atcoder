e0fa8ce8f87b36d02e3287b5b61f63588a306fdc
соджиро кояма <koyama10032009@outlook.jp>
2022-11-28 00:15:45 +0300

    abc279 - e

--- abc/abc279/src/bin/e.rs
+++ abc/abc279/src/bin/e.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; m],
+    }
+    let mut b = (0..=n).collect::<Vec<usize>>();
+    for i in 0..m {
+        b.swap(a[i], a[i] + 1);
+    }
+    let mut pos = vec![0; n + 1];
+    for j in 1..=n {
+        pos[b[j]] = j;
+    }
+
+    let mut b = (0..=n).collect::<Vec<usize>>();
+    for i in 0..m {
+        if b[a[i]] == 1 {
+            println!("{}", pos[b[a[i] + 1]]);
+        } else if b[a[i] + 1] == 1 {
+            println!("{}", pos[b[a[i]]]);
+        } else {
+            println!("{}", pos[1]);
+        }
+        b.swap(a[i], a[i] + 1);
+    }
 }
