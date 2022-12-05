8ed1ef83d5dbb1e37cfe9d287e9710d169e3aff2
соджиро кояма <koyama10032009@outlook.jp>
2022-12-05 09:52:00 +0300

    abc280 - d

--- abc/src/abc280.rs
+++ abc/src/abc280.rs
@@ -6,6 +6,34 @@ use proconio::{
 #[fastout]
 pub fn main() {
     input! {
+        mut k: usize,
+    }
+    let mut ans = 1usize;
+    let mut p = 2;
+    while p * p <= k {
+        let mut a = 0;
+        while k % p == 0 {
+            k /= p;
+            a += 1;
+        }
+
+        let mut n = 0;
+        while a > 0 {
+            n += p;
+            let mut x = n;
+            while x % p == 0 {
+                x /= p;
+                a -= 1;
+            }
+        }
+        ans = ans.max(n);
+        p += 1;
+    }
+    ans = ans.max(k);
+    println!("{}", ans);
+}
+fn c() {
+    input! {
         k: Chars,
         t: Chars,
     };
