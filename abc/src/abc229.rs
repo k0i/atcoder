f95980d167dcf83ba5ebe0dd96b4036df0eeda7b
соджиро кояма <koyama10032009@outlook.jp>
2022-12-14 12:06:56 +0300

    abc229 - e

--- abc/src/abc229.rs
+++ abc/src/abc229.rs
@@ -8,6 +8,82 @@ use std::ops::{Add, Sub};
 #[fastout]
 pub fn main() {
     input! {
+    n:usize,
+    m:usize,
+    ab:[(Usize1,Usize1);m]
+    }
+    let mut graph = vec![vec![]; n];
+    for (a, b) in ab {
+        graph[a].push(b);
+    }
+    let mut uf = UnionFindTree::new(n);
+    let mut ans = 0;
+    let mut res = vec![0];
+    for i in (1..n).rev() {
+        ans += 1;
+        for &j in &graph[i] {
+            if uf.is_same(i, j) {
+                continue;
+            }
+            uf.unite(i, j);
+            ans -= 1;
+        }
+        res.push(ans);
+    }
+    res.reverse();
+    for i in 0..n {
+        println!("{}", res[i]);
+    }
+}
+
+#[derive(Debug)]
+pub struct UnionFindTree {
+    parents: Vec<isize>,
+    size: Vec<usize>,
+}
+
+impl UnionFindTree {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parents: vec![-1; n],
+            size: vec![1; n],
+        }
+    }
+    pub fn root(&mut self, x: usize) -> usize {
+        if self.parents[x] == -1 {
+            x
+        } else {
+            self.parents[x] = self.root(self.parents[x] as usize) as isize;
+            self.parents[x] as usize
+        }
+    }
+
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+    pub fn unite(&mut self, x: usize, y: usize) -> bool {
+        let mut x = self.root(x);
+        let mut y = self.root(y);
+
+        if x == y {
+            return false;
+        }
+
+        if self.size[x] < self.size[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.parents[y] = x as isize;
+        self.size[x] += self.size[y];
+        true
+    }
+
+    pub fn size(&mut self, x: usize) -> usize {
+        let r = self.root(x);
+        self.size[r]
+    }
+}
+fn d() {
+    input! {
     s:Chars,
     k:i64
         }
