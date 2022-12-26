b30c22701548881401afc012a4e34aac71149efa
соджиро кояма <koyama10032009@outlook.jp>
2022-12-26 03:22:51 +0300

    abc283 - review

--- abc/abc283/src/bin/c.rs
+++ abc/abc283/src/bin/c.rs
@@ -7,32 +7,8 @@ use std::collections::{HashMap, HashSet};
 #[fastout]
 pub fn main() {
     input! {
-    mut s:Chars
-        }
-
-    let mut zero = false;
-    let mut ans = 0;
-    for i in s {
-        match i {
-            '0' => {
-                if zero {
-                    ans += 1;
-                    zero = false;
-                    continue;
-                }
-                zero = true;
-            }
-            _ => {
-                if zero {
-                    zero = false;
-                    ans += 1;
-                }
-                ans += 1;
-            }
-        }
+    mut s:String
     }
-    if zero {
-        ans += 1;
-    }
-    println!("{}", ans);
+    s = s.replace("00", "0");
+    println!("{}", s.len());
 }
--- abc/abc283/src/bin/d.rs
+++ abc/abc283/src/bin/d.rs
@@ -1,15 +1,22 @@
+use im_rc::HashSet;
+
 fn main() {
     proconio::input! {
     s:proconio::marker::Chars
         }
     let mut a = std::collections::HashSet::new();
+    let mut stacks = vec![HashSet::new()];
     for i in 0..s.len() {
         match s[i] {
             '(' => {
+                stacks.push(HashSet::new());
                 continue;
             }
             ')' => {
-                a.clear();
+                let stack = stacks.pop().unwrap();
+                for x in stack.iter() {
+                    a.remove(x);
+                }
             }
             _ => {
                 if a.contains(&s[i]) {
@@ -17,6 +24,9 @@ fn main() {
                     return;
                 }
                 a.insert(s[i]);
+                let mut stack = stacks.pop().unwrap();
+                stack.insert(s[i]);
+                stacks.push(stack);
             }
         }
     }
