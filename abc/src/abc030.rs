7bdca0397173e2999d3ddb5dcc348f354e10a201
соджиро кояма <koyama10032009@outlook.jp>
2022-12-29 17:06:15 +0300

    abc030 - c

--- abc/src/abc030.rs
+++ abc/src/abc030.rs
@@ -3,8 +3,102 @@ use proconio::{
     fastout, input,
     marker::{Bytes, Chars, Isize1, Usize1},
 };
-#[fastout]
 pub fn main() {
+    input! {
+    n:usize,
+    m:usize,
+    x:usize,
+    y:usize,
+    a:[usize;n],
+    b:[usize;m],
+       }
+    let mut time = 0;
+    let mut ans = 0;
+    let mut a_to_b = true;
+    'outer: loop {
+        if a_to_b {
+            match a.lower_bound(time) {
+                Ok(i) => {
+                    time = a[i] + x;
+                    a_to_b = false;
+                }
+                Err(_) => {
+                    break 'outer;
+                }
+            }
+        } else {
+            match b.lower_bound(time) {
+                Ok(i) => {
+                    time = b[i] + y;
+                    ans += 1;
+                    a_to_b = true;
+                }
+                Err(_) => {
+                    break 'outer;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
+}
+
+use std::cmp::Ordering;
+
+pub trait Bound<T> {
+    fn lower_bound(&self, x: T) -> Result<usize, usize>;
+    fn upper_bound(&self, x: T) -> Result<usize, usize>;
+}
+
+impl<T: Ord> Bound<T> for [T] {
+    fn lower_bound(&self, x: T) -> Result<usize, usize> {
+        if self.is_empty() {
+            return Err(0);
+        }
+        if self.iter().last().unwrap() < &x {
+            return Err(self.len());
+        }
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low < high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(&x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                _ => {
+                    high = mid;
+                }
+            }
+        }
+        Ok(low)
+    }
+    fn upper_bound(&self, x: T) -> Result<usize, usize> {
+        if self.is_empty() {
+            return Err(0);
+        }
+        if self.iter().last().unwrap() <= &x {
+            return Err(self.len());
+        }
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low < high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(&x) {
+                Ordering::Greater => {
+                    high = mid;
+                }
+                _ => {
+                    low = mid + 1;
+                }
+            }
+        }
+        Ok(low)
+    }
+}
+
+fn b() {
     input! {
         n: f64,
         m: f64,
--- abc/src/main.rs
+++ abc/src/main.rs
@@ -183,5 +183,5 @@ mod pa_ken2019;
 mod panasonic2020;
 mod zone2021;
 fn main() {
-    abc212::main();
+    abc030::main();
 }
