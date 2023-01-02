3819cd4d04d0d734c4e183752a4ea6b2fe3377aa
соджиро кояма <koyama10032009@outlook.jp>
2023-01-02 03:28:34 +0300

    abc051 - c

--- abc/src/abc051.rs
+++ abc/src/abc051.rs
@@ -7,6 +7,20 @@ use std::collections::{HashMap, HashSet};
 #[fastout]
 pub fn main() {
     input! {
+      sx: isize, sy: isize, tx: isize, ty: isize
+    }
+
+    let dx = (tx - sx) as usize;
+    let dy = (ty - sy) as usize;
+    let path1 = "R".repeat(dx) + &"U".repeat(dy);
+    let path2 = "L".repeat(dx) + &"D".repeat(dy);
+    let path3 = "D".to_owned() + &"R".repeat(dx + 1) + &"U".repeat(dy + 1) + "L";
+    let path4 = "U".to_owned() + &"L".repeat(dx + 1) + &"D".repeat(dy + 1) + "R";
+
+    println!("{}", path1 + &path2 + &path3 + &path4);
+}
+fn b() {
+    input! {
     k:usize,
     s:usize
         }
