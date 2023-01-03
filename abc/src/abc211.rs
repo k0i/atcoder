a5b5ae85e825788d0c89bc3690b7939a81967d6f
соджиро кояма <koyama10032009@outlook.jp>
2023-01-03 06:26:41 +0300

    abc211 - d

--- abc/src/abc211.rs
+++ abc/src/abc211.rs
@@ -6,6 +6,38 @@ use proconio::{
 #[fastout]
 pub fn main() {
     input! {
+    n:usize,
+    m:usize,
+    ab:[(Usize1,Usize1);m],
+        }
+    let mut tree = vec![vec![]; n];
+    for i in 0..m {
+        let (a, b) = ab[i];
+        tree[a].push(b);
+        tree[b].push(a);
+    }
+    let mut dist = vec![0; n];
+    let mut shortest_path_cnt = vec![0; n];
+    shortest_path_cnt[0] = 1;
+    let mut queue = std::collections::VecDeque::new();
+    queue.push_back(0);
+    while !queue.is_empty() {
+        let v = queue.pop_front().unwrap();
+        for &u in &tree[v] {
+            if dist[u] == 0 {
+                dist[u] = dist[v] + 1;
+                shortest_path_cnt[u] = shortest_path_cnt[v];
+                queue.push_back(u);
+            } else if dist[u] == dist[v] + 1 {
+                shortest_path_cnt[u] += shortest_path_cnt[v];
+                shortest_path_cnt[u] %= 1_000_000_007;
+            }
+        }
+    }
+    println!("{}", shortest_path_cnt[n - 1]);
+}
+fn c() {
+    input! {
     s:Chars
         }
     let ch = ['c', 'h', 'o', 'k', 'u', 'd', 'a', 'i'];
