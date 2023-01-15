f52296cbe966750587eac7477219b9961a47d10e
соджиро кояма <koyama10032009@outlook.jp>
2023-01-15 03:43:44 +0300

    arc114 - b

--- abc/src/arc114.rs
+++ abc/src/arc114.rs
@@ -4,12 +4,45 @@ use proconio::{
     marker::{Bytes, Chars, Isize1, Usize1},
 };
 use std::collections::{HashMap, HashSet};
-#[fastout]
 pub fn main() {
+    input! {
+        n: usize,
+        fs: [usize; n],
+    }
+
+    let mut result = 1;
+
+    let mut checked: Vec<usize> = vec![0; n];
+
+    for i in 0..n {
+        if checked[i] != 0 {
+            continue;
+        }
+
+        let mut j = i;
+
+        while checked[j] == 0 {
+            checked[j] = i + 1;
+            j = fs[j] - 1;
+        }
+
+        if checked[j] == i + 1 {
+            result *= 2;
+            result %= 998244353;
+        }
+    }
+
+    result += 998244353;
+    result -= 1;
+    result %= 998244353;
+    println!("{}", result);
+}
+
+fn a() {
     input! {
     n:usize,
     x:[u64;n]
-        }
+    }
 
     let v = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
     let mut ans = std::u64::MAX;
