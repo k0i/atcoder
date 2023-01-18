9d6382c770e93d9b20bac2d0aaa57316a71086da
соджиро кояма <koyama10032009@outlook.jp>
2023-01-18 04:38:37 +0300

    abc195 - d

--- abc/src/abc195.rs
+++ abc/src/abc195.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 use itertools::Itertools;
 use proconio::{
     fastout, input,
@@ -5,6 +7,40 @@ use proconio::{
 };
 #[fastout]
 pub fn main() {
+    d()
+}
+
+fn d() {
+    input! {
+        n: usize,
+        m: usize,
+        q: usize,
+        wv: [(i64, i64); n],
+        x: [i64; m],
+        lr: [(Usize1, usize); q],
+    }
+    for &(l, r) in &lr {
+        let mut ev = vec![];
+        for i in (0..l).chain(r..m) {
+            ev.push((x[i], 1, 0));
+        }
+        for (i, &(w, v)) in wv.iter().enumerate() {
+            ev.push((w, 0, v))
+        }
+        ev.sort();
+        let mut res = 0;
+        let mut pq = BinaryHeap::new();
+        for &(w, t, v) in &ev {
+            if t == 0 {
+                pq.push(v);
+            } else if let Some(v) = pq.pop() {
+                res += v;
+            }
+        }
+        println!("{}", res);
+    }
+}
+fn b() {
     input! {
     a:i64,
     b:i64,
