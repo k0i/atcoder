ee665c4cfee7b45ee5b03f817d29a3d18ff0e6a1
соджиро кояма <koyama10032009@outlook.jp>
2023-01-20 02:03:35 +0300

    abc262 - d

--- abc/src/abc262.rs
+++ abc/src/abc262.rs
@@ -3,8 +3,34 @@ use proconio::{
     fastout, input,
     marker::{Bytes, Chars, Isize1, Usize1},
 };
+const M: i64 = 998244353;
 #[fastout]
 pub fn main() {
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+    let mut ans: i64 = 0;
+    for i in 1..=n {
+        let mut dp = vec![vec![0; i]; i + 1];
+        dp[0][0] = 1;
+        for &ai in &a {
+            let old_dp = dp.clone();
+            for j in 0..i {
+                for k in 0..i {
+                    let new_k = (ai + k) % i;
+                    dp[j + 1][new_k] += old_dp[j][k];
+                    dp[j + 1][new_k] %= M;
+                }
+            }
+        }
+        ans += dp[i][0];
+        ans %= M;
+    }
+
+    println!("{}", ans);
+}
+fn c() {
     input! {
     n:usize,
     mut a:[usize;n]
