b0f9f8bd66800f0c336883db3ce467347d6bc283
соджиро кояма <koyama10032009@outlook.jp>
2023-01-21 08:40:49 +0300

    abc048 - c

--- abc/src/abc048.rs
+++ abc/src/abc048.rs
@@ -5,6 +5,26 @@ use proconio::{
 };
 #[fastout]
 pub fn main() {
+    input! {
+    n:usize,
+    x:usize,
+    mut a:[usize;n]
+    }
+    let mut ans = 0;
+
+    for i in 0..n - 1 {
+        if x < a[i] {
+            ans += a[i] - x;
+            a[i] = x;
+        }
+        if x < a[i] + a[i + 1] {
+            ans += a[i] + a[i + 1] - x;
+            a[i + 1] = x - a[i];
+        }
+    }
+    println!("{}", ans);
+}
+fn b() {
     input! {a:usize,b:usize,x:usize}
     if a == 0 {
         println!("{}", div(b, x) + 1);
--- abc/src/main.rs
+++ abc/src/main.rs
@@ -213,5 +213,5 @@ mod panasonic2020;
 mod sumitob2019;
 mod zone2021;
 fn main() {
-    abc162::main();
+    abc048::main();
 }
