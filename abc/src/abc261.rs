b153c8e7857da460e47f1d0dcdf2e8cf979ed521
соджиро кояма <koyama10032009@outlook.jp>
2023-02-02 02:04:20 +0300

    abc261 - e

--- abc/src/abc261.rs
+++ abc/src/abc261.rs
@@ -25,7 +25,36 @@ macro_rules! chmin {
 }
 
 pub fn main() {
-    c()
+    e()
+}
+
+fn e() {
+    input! {
+        n:usize,
+        c:usize,
+        ta:[(usize,usize);n],
+    }
+    let max = (1 << 30) - 1;
+    let mut ans = c;
+    let mut zero = 0;
+    let mut one = max;
+    for (t, a) in ta {
+        if t == 1 {
+            zero &= a;
+            one &= a;
+        }
+        if t == 2 {
+            zero |= a;
+            one |= a;
+        }
+        if t == 3 {
+            zero ^= a;
+            one ^= a;
+        }
+        let next = ((ans ^ max) & zero) + (ans & one);
+        println!("{}", next);
+        ans = next;
+    }
 }
 
 fn d() {
