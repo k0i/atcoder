67b1c93a15c304e928e08ac1951a9eefb07ba636
соджиро кояма <koyama10032009@outlook.jp>
2023-02-06 02:06:41 +0300

    abc244 - e

--- abc/src/abc244.rs
+++ abc/src/abc244.rs
@@ -1,9 +1,41 @@
-use std::collections::HashSet;
-
+#![allow(unused_imports)]
 use itertools::Itertools;
-use proconio::{input, source::line::LineSource};
+use proconio::{fastout, input, source::line::LineSource};
+use std::collections::{HashMap, HashSet};
 use std::io::{stdin, stdout, BufReader, Write};
+#[fastout]
 pub fn main() {
+    proconio::input! {
+        n: usize, m: usize, k: usize,
+        s: usize, t: usize, x: usize,
+        v: [(usize, usize); m],
+    };
+
+    const MOD: usize = 998244353;
+
+    let mut g = vec![Vec::new(); n + 1];
+    v.iter().for_each(|&root| {
+        g[root.0].push(root.1);
+        g[root.1].push(root.0);
+    });
+
+    let mut dp = vec![vec![vec![0usize; 2]; n + 1]; k + 1]; // dp[K][N][%2]
+    dp[0][s][0] = 1;
+    for i in 0..k {
+        for j in 1..=n {
+            for &l in g[j].iter() {
+                for ll in 0..2 {
+                    let rev = if x == l { 1 } else { 0 };
+                    dp[i + 1][l][(ll + rev) % 2] += dp[i][j][ll];
+                    dp[i + 1][l][(ll + rev) % 2] %= MOD;
+                }
+            }
+        }
+    }
+
+    println!("{}", dp[k][t][0]);
+}
+fn d() {
     let stdin = stdin();
     let mut source = LineSource::new(BufReader::new(stdin.lock()));
     input! {
