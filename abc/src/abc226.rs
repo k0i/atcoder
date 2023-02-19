783c466121775591844f0ff1d39974ccacb9d6d5
соджиро кояма <koyama10032009@outlook.jp>
2023-02-19 04:39:36 +0300

    abc226 - e

--- abc/src/abc226.rs
+++ abc/src/abc226.rs
@@ -9,6 +9,63 @@ use proconio::{
 #[fastout]
 pub fn main() {
     input! {
+        n: usize,
+        m: usize,
+        uv: [(usize, usize); m]
+    }
+
+    let mut list = vec![vec![]; n];
+    for (i, (u, v)) in uv.into_iter().enumerate() {
+        list[u - 1].push((v - 1, i));
+        list[v - 1].push((u - 1, i));
+    }
+
+    let mut r = 1;
+    let mut seen = vec![false; n];
+    let mut used = vec![false; m];
+    for i in 0..n {
+        if seen[i] {
+            continue;
+        }
+
+        let mut cycle = 0;
+
+        dfs(i, &list, &mut seen, &mut used, &mut cycle);
+
+        if cycle == 1 {
+            r *= 2;
+            r %= 998244353;
+        } else {
+            println!("0");
+            return;
+        }
+    }
+    println!("{}", r);
+}
+
+fn dfs(
+    i: usize,
+    list: &Vec<Vec<(usize, usize)>>,
+    seen: &mut Vec<bool>,
+    used: &mut Vec<bool>,
+    cycle: &mut usize,
+) {
+    seen[i] = true;
+    for (next, j) in &list[i] {
+        if used[*j] {
+            continue;
+        }
+        used[*j] = true;
+        if seen[*next] {
+            *cycle += 1;
+            continue;
+        }
+        dfs(*next, list, seen, used, cycle);
+    }
+}
+
+fn d() {
+    input! {
     n:usize,
     mut a:[(i64,i64);n]
         }
