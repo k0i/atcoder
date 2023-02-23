78455c6df36ac1065fab30703c5d977ca2047598
соджиро кояма <koyama10032009@outlook.jp>
2023-02-23 11:45:49 +0300

    panasonic2020 - d

--- abc/src/panasonic2020.rs
+++ abc/src/panasonic2020.rs
@@ -7,6 +7,24 @@ use std::collections::{HashMap, HashSet};
 #[fastout]
 pub fn main() {
     input! {
+        n: usize,
+    };
+    f(&mut String::new(), n, b'a');
+}
+fn f(s: &mut String, n: usize, c: u8) {
+    if s.len() == n {
+        println!("{}", s);
+        return;
+    }
+    for i in b'a'..=c {
+        s.push(i as char);
+        f(s, n, c + if i == c { 1 } else { 0 });
+        s.pop();
+    }
+}
+
+fn c() {
+    input! {
         a: i64,
         b: i64,
         c: i64,
