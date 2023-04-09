67decec0f8ca47cd723e2ccb071ef3816fb299e7
соджиро кояма <koyama10032009@outlook.jp>
2023-04-09 08:20:00 +0300

    caddi2018 - c

--- abc/src/caddi2018.rs
+++ abc/src/caddi2018.rs
@@ -7,6 +7,30 @@ use proconio::{
 use std::collections::{HashMap, HashSet};
 #[fastout]
 pub fn main() {
+    c()
+}
+
+fn c() {
+    ip! {
+      n:u64,
+      mut p:u64,
+    }
+    let mut ans = 1;
+    let mut l: u64 = 2;
+    for i in 2..1000000 {
+        let mut x: u64 = 0;
+        while p % i == 0 {
+            x += 1;
+            p /= i;
+        }
+        ans *= i.pow((x / n) as u32);
+        l += 1;
+    }
+    ans *= p.pow((1 / n) as u32);
+    println!("{}", ans);
+}
+
+fn d() {
     ip! {
     n:usize,
     a:[usize;n],
