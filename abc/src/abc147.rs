e93abefeb824e72c0acf4e10d598af8fd1a5784c
соджиро кояма <koyama10032009@outlook.jp>
2023-05-07 06:49:42 +0300

    abc147 - d

--- abc/src/abc147.rs
+++ abc/src/abc147.rs
@@ -5,6 +5,39 @@ use proconio::{
 };
 #[fastout]
 pub fn main() {
+    d()
+}
+
+fn d() {
+    input! {
+        n: usize,
+        a: [u64; n],
+    };
+    let mut zeros: Vec<u64> = vec![0; 61];
+    let mut ones: Vec<u64> = vec![0; 61];
+
+    for i in 0..n {
+        for j in 0..61 {
+            if a[i] & (1 << j) == 0 {
+                zeros[j] += 1;
+            } else {
+                ones[j] += 1;
+            }
+        }
+    }
+
+    const MOD: u64 = 1000000007;
+    let mut res = 0;
+    for i in 0..61 {
+        let scale = (1_u64 << i) % MOD;
+        let mut r = (ones[i] * scale) % MOD;
+        r = (zeros[i] * r) % MOD;
+        res = (res + r) % MOD;
+    }
+    println!("{}", res);
+}
+
+fn c() {
     input! {
     n:usize,
         }
--- abc/src/main.rs
+++ abc/src/main.rs
@@ -277,5 +277,5 @@ mod tenka1_2018;
 mod tenka1_2019;
 mod zone2021;
 fn main() {
-    abc132::main();
+    abc147::main();
 }
