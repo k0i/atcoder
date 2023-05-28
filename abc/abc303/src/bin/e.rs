471feb7f0c7043bd6fbd053e9aa5783908a58c9f
соджиро кояма <koyama10032009@outlook.jp>
2023-05-28 09:53:20 +0300

    abc303 - e

--- abc/abc303/src/bin/e.rs
+++ abc/abc303/src/bin/e.rs
@@ -8,7 +8,7 @@ use proconio::{
     fastout, input as ip,
     marker::{Bytes, Chars, Isize1, Usize1 as U1},
 };
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use superslice::*;
 #[fastout]
 pub fn main() {
@@ -21,4 +21,19 @@ pub fn main() {
         g[u].push(v);
         g[v].push(u);
     }
+    let mut ans = vec![];
+    let mut lish = n;
+
+    for i in 0..n {
+        if g[i].len() > 2 {
+            ans.push(g[i].len());
+            lish -= g[i].len() + 1;
+        }
+    }
+    for _ in 0..lish / 3 {
+        ans.push(2);
+    }
+    ans.sort();
+
+    println!("{}", ans.iter().join(" "));
 }
